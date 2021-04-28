1e15fd9b8f40d02e9755847328b613ca21d27697
carloseduardodb <carloscangere@hotmail.com>
2021-04-27 21:01:40 -0300

    fix: add configuration tailwindcss for deploy in vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -44,5 +44,20 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    "tailwindcss",
+    "postcss-flexbugs-fixes",
+    [
+      "postcss-preset-env",
+      {
+        autoprefixer: {
+          flexbox: "no-2009",
+        },
+        stage: 3,
+        features: {
+          "custom-properties": false,
+        },
+      },
+    ],
+  ],
 };

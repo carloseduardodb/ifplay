c9958d39a1dcf77384f66d0e685a4cf8b7bdd869
carloseduardodb <carloscangere@hotmail.com>
2021-04-29 14:44:32 -0300

    fix(tailwind.config.js): remove sources to purge css for development mode
    when it goes into production it is necessary to add the path and solve the bug. This is just a
    palliative solution.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  purge: [
-    ".src/pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  purge: [] /* add purge to production mode */,
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {

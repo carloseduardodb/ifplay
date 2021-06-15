629699e987824670f912cc8bcd35fa0fa3c2b6ea
carloseduardodb <carloscangere@hotmail.com>
2021-06-15 14:47:58 -0300

    style: add bg color in HeaderStats

--- src/components/Dashboard/HeaderStats/index.tsx
+++ src/components/Dashboard/HeaderStats/index.tsx
@@ -8,7 +8,7 @@ export default function HeaderStats() {
   return (
     <>
       {/* Header */}
-      <div className="relative bg-blueGray-800 md:pt-32 pb-16 xs:pb-1 pt-12">
+      <div className="relative bg-p-black-dark md:pt-32 pb-16 xs:pb-1 pt-12">
         <div className="px-4 md:px-10 mx-auto w-full">
           <div>
             {/* Card stats */}

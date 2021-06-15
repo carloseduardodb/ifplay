b1fc1ae8288ce1a6b29c3b6ebeb3bd11c6d1a81a
carloseduardodb <carloscangere@hotmail.com>
2021-06-15 12:24:40 -0300

    refactor: remove scrollbars extension from the list

--- src/components/Videos/PlayTrack/ListVideos/index.tsx
+++ src/components/Videos/PlayTrack/ListVideos/index.tsx
@@ -1,48 +1,29 @@
 import React from "react";
 import DetailsVideoList from "./DetailsVideoList";
-import { Scrollbars } from "react-custom-scrollbars";
 import DetailsQuestions from "./DetailsQuestions";
-
-const renderThumb = ({ style, ...props }) => {
-  const thumbStyle = {
-    borderRadius: 6,
-    backgroundColor: "#45c8d4",
-  };
-  return <div style={{ ...style, ...thumbStyle }} {...props} />;
-};
-
-const CustomScrollbars = (props) => (
-  <Scrollbars
-    renderThumbHorizontal={renderThumb}
-    renderThumbVertical={renderThumb}
-    {...props}
-  />
-);
 
 const ListVideos = () => {
   return (
-    <ul className="w-4/12 overflow-hidden">
-      <CustomScrollbars autoHide autoHideTimeout={500} autoHideDuration={200}>
-        <DetailsVideoList selected />
-        <DetailsVideoList />
-        <DetailsVideoList />
-        <DetailsVideoList />
-        <DetailsVideoList />
-        <DetailsVideoList />
-        <DetailsVideoList />
-        <DetailsVideoList />
-        <DetailsVideoList />
-        <DetailsVideoList />
-        <DetailsVideoList />
-        <DetailsVideoList />
-        <DetailsVideoList />
-        <DetailsVideoList />
-        <DetailsVideoList />
-        <DetailsVideoList />
-        <DetailsVideoList />
-        <DetailsVideoList />
-        <DetailsQuestions />
-      </CustomScrollbars>
+    <ul className="w-4/12 overflow-hidden scrollbar scrollbar-thumb-p-green scrollbar-thin scrollbar-track-gray-800">
+      <DetailsVideoList selected />
+      <DetailsVideoList />
+      <DetailsVideoList />
+      <DetailsVideoList />
+      <DetailsVideoList />
+      <DetailsVideoList />
+      <DetailsVideoList />
+      <DetailsVideoList />
+      <DetailsVideoList />
+      <DetailsVideoList />
+      <DetailsVideoList />
+      <DetailsVideoList />
+      <DetailsVideoList />
+      <DetailsVideoList />
+      <DetailsVideoList />
+      <DetailsVideoList />
+      <DetailsVideoList />
+      <DetailsVideoList />
+      <DetailsQuestions />
     </ul>
   );
 };

96da251b9e0e4d543e6f700376b0135bcb047007
carloseduardodb <carloscangere@hotmail.com>
2021-06-15 16:46:14 -0300

    feat: add CardTable

---
+++ src/components/Dashboard/CardTable/index.tsx
@@ -0,0 +1,144 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+// components
+
+import TableDropdown from "../TableDropdown";
+import { FiUser } from "react-icons/fi";
+import { FaCircle, FaEye, FaUserCircle } from "react-icons/fa";
+
+export default function CardTable({ color }) {
+  return (
+    <>
+      <div
+        className={
+          "relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded " +
+          (color === "light" ? "bg-white" : "bg-blue-700 text-white")
+        }
+      >
+        <div className="rounded-t mb-0 px-4 py-3 border-0">
+          <div className="flex flex-wrap items-center">
+            <div className="relative w-full px-4 max-w-full flex-grow flex-1">
+              <h3
+                className={
+                  "font-semibold text-lg " +
+                  (color === "light" ? "text-blueGray-700" : "text-white")
+                }
+              >
+                Últimas Respostas
+              </h3>
+            </div>
+          </div>
+        </div>
+        <div className="block w-full overflow-x-auto">
+          {/* Projects table */}
+          <table className="items-center w-full bg-transparent border-collapse">
+            <thead>
+              <tr>
+                <th
+                  className={
+                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
+                    (color === "light"
+                      ? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
+                      : "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
+                  }
+                >
+                  Nomes
+                </th>
+                <th
+                  className={
+                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
+                    (color === "light"
+                      ? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
+                      : "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
+                  }
+                >
+                  Emails
+                </th>
+                <th
+                  className={
+                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
+                    (color === "light"
+                      ? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
+                      : "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
+                  }
+                >
+                  Acertos
+                </th>
+                <th
+                  className={
+                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
+                    (color === "light"
+                      ? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
+                      : "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
+                  }
+                >
+                  Erros
+                </th>
+                <th
+                  className={
+                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
+                    (color === "light"
+                      ? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
+                      : "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
+                  }
+                >
+                  Ação
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left flex items-center">
+                  <span className="h-12 w-12 bg-blue-400 rounded-full border flex justify-center items-center">
+                    <FaUserCircle size={35} />
+                  </span>
+                  <span
+                    className={
+                      "ml-3 font-bold " +
+                      +(color === "light" ? "text-blueGray-600" : "text-white")
+                    }
+                  >
+                    Carlos Eduardo Dias Batista
+                  </span>
+                </th>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                  carlos@gmail.com
+                </td>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                  <div className="flex">
+                    <FaCircle color="green" className="mr-2" /> <span>8</span>
+                  </div>
+                </td>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                  <div className="flex">
+                    <FaCircle color="red" className="mr-2" /> <span>2</span>
+                  </div>
+                </td>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                  <div className="flex items-center">
+                    <a
+                      href="#"
+                      className="bg-blue-500 px-5 py-3 rounded text-white flex gap-x-2 justify-center items-center hover:bg-blue-800"
+                    >
+                      <FaEye />
+                      <span>Visualizar</span>
+                    </a>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  );
+}
+
+CardTable.defaultProps = {
+  color: "light",
+};
+
+CardTable.propTypes = {
+  color: PropTypes.oneOf(["light", "dark"]),
+};
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import CardTable from "../../components/Dashboard/CardTable";
 import Navbar from "../../components/Dashboard/Navbar";
 import Sidebar from "../../components/Dashboard/Sidebar";
 import Admin from "../../layouts/Admin";
@@ -7,8 +8,10 @@ export default function Dashboard() {
   return (
     <>
       <Admin>
-        <div className="">
-          <p className="text-white">A new posting message</p>
+        <div className="flex flex-wrap -mt-10">
+          <div className="w-full mb-12 px-4">
+            <CardTable />
+          </div>
         </div>
       </Admin>
     </>

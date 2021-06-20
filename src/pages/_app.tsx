584076b4d08f34fed5978f3a96f10c34bb515536
carloseduardodb <carloscangere@hotmail.com>
2021-06-20 19:52:07 -0300

    feat: a conditional to render landing page and dashboard to statick pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,40 @@
 import "./../styles/globals.scss";
 import "tailwindcss/tailwind.css";
+import { useRouter } from "next/router";
+import Head from "next/head";
+import Sidebar from "../components/Dashboard/Sidebar";
+import Navbar from "../components/Dashboard/Navbar";
+import HeaderStats from "../components/Dashboard/HeaderStats";
+import FooterAdmin from "../components/Dashboard/Footer";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <div>
-      <Component {...pageProps} />
-    </div>
-  );
+  const router = useRouter();
+  //display dashboard
+  if (router.pathname.indexOf("/dashboard") !== -1) {
+    return (
+      <div>
+        <Head>
+          <title>IF (PLAY) - Dashboard</title>
+        </Head>
+        <Sidebar />
+        <div className="relative md:ml-64 flex flex-col min-h-screen justify-between">
+          <div className="flex flex-col">
+            <Navbar />
+            <Component {...pageProps} />
+            <FooterAdmin />
+          </div>
+        </div>
+      </div>
+    );
+  }
+  //landing page and login and logout page display
+  else if (router.pathname === "/") {
+    return (
+      <div>
+        <Component {...pageProps} />
+      </div>
+    );
+  }
 }
 
 export default MyApp;

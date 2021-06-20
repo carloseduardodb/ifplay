73aa0469cedddd68b0a3f67d8e57b83a6da45bcf
carloseduardodb <carloscangere@hotmail.com>
2021-06-20 19:53:50 -0300

    chore: remove admin tag to component

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,19 +1,19 @@
 import React from "react";
+import { FaChevronDown } from "react-icons/fa";
+import CardStats from "../../components/Dashboard/CardStats";
 import CardTable from "../../components/Dashboard/CardTable";
-import Navbar from "../../components/Dashboard/Navbar";
-import Sidebar from "../../components/Dashboard/Sidebar";
-import Admin from "../../layouts/Admin";
+import HeaderStats from "../../components/Dashboard/HeaderStats";
+import PlaylistCard from "../../components/Dashboard/PlaylistCard";
 
 export default function Dashboard() {
   return (
     <>
-      <Admin>
-        <div className="flex flex-wrap -mt-10">
-          <div className="w-full mb-12 px-4">
-            <CardTable />
-          </div>
+      <HeaderStats />
+      <div className="relative pb-16 xs:pb-1 -top-8">
+        <div className="px-4 md:px-16 mx-auto w-full">
+          <CardTable />
         </div>
-      </Admin>
+      </div>
     </>
   );
 }

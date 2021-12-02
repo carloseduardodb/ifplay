c606a80843979df3bc8483c384abef6f425b3a1f
carloseduardodb <carloseduardodiasbatista@gmail.com>
2021-12-01 22:43:51 -0300

    feat: add template in app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,13 @@
 import "./../styles/globals.scss";
 import "tailwindcss/tailwind.css";
 import { useRouter } from "next/router";
-import Head from "next/head";
-import Sidebar from "../components/Dashboard/Sidebar";
-import Navbar from "../components/Dashboard/Navbar";
-import FooterAdmin from "../components/Dashboard/Footer";
 import { AuthProvider } from "../contexts/AuthContext";
 import { useEffect } from "react";
 import Router from "next/router";
 import { useAuth } from "../hooks/useAuth";
+import { DispatchProvider } from "../contexts/DispatchEventContext";
+import AdminLayout from "../layouts/AdminLayout";
+import DefaultLayout from "../layouts/DefaultLayout";
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
@@ -24,23 +23,17 @@ function MyApp({ Component, pageProps }) {
   //display dashboard
   return (
     <AuthProvider>
-      {router.pathname.indexOf("/dashboard") !== -1 ? (
-        <>
-          <Head>
-            <title>IF (PLAY) - Dashboard</title>
-          </Head>
-          <Sidebar />
-          <div className="relative md:ml-64 flex flex-col min-h-screen justify-between">
-            <div className="flex flex-col">
-              <Navbar />
-              <Component {...pageProps} />
-              <FooterAdmin />
-            </div>
-          </div>
-        </>
-      ) : (
-        <Component {...pageProps} />
-      )}
+      <DispatchProvider>
+        {router.pathname.includes("dashboard") ? (
+          <AdminLayout>
+            <Component {...pageProps} />
+          </AdminLayout>
+        ) : (
+          <DefaultLayout>
+            <Component {...pageProps} />
+          </DefaultLayout>
+        )}
+      </DispatchProvider>
     </AuthProvider>
   );
 }

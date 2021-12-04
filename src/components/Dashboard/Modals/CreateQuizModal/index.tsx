e1bdf44c5847ccfa91f0c4b5cb9f4b8d290d9737
carloseduardodb <carloseduardodiasbatista@gmail.com>
2021-12-04 19:00:23 -0300

    feat: prepare form for submit

--- src/components/Dashboard/Modals/CreateQuizModal/index.tsx
+++ src/components/Dashboard/Modals/CreateQuizModal/index.tsx
@@ -79,7 +79,10 @@ export default function CreateQuizModal() {
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+              <form
+                onSubmit={handleSubmit}
+                className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
+              >
                 <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                   <div className="sm:flex sm:items-start">
                     <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-blue-100 sm:mx-0 sm:h-10 sm:w-10">
@@ -111,7 +114,7 @@ export default function CreateQuizModal() {
                 </div>
                 <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
                   <button
-                    type="button"
+                    type="submit"
                     className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
                     onClick={() => {}}
                   >
@@ -126,7 +129,7 @@ export default function CreateQuizModal() {
                     Cancelar
                   </button>
                 </div>
-              </div>
+              </form>
             </Transition.Child>
           </div>
         </Dialog>

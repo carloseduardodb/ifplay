f32973efb45e02c83abf2e643907b7f0c7c68db0
carloseduardodb <carloseduardodiasbatista@gmail.com>
2021-12-04 19:06:04 -0300

    feat: add default methods for submid forms

--- src/components/Dashboard/Modals/CreateTeamModal/index.tsx
+++ src/components/Dashboard/Modals/CreateTeamModal/index.tsx
@@ -83,7 +83,10 @@ export default function CreateTeamModal() {
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+              <form
+                onSubmit={handleSubmit}
+                className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
+              >
                 <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                   <div className="sm:flex sm:items-start">
                     <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-blue-100 sm:mx-0 sm:h-10 sm:w-10">
@@ -121,9 +124,8 @@ export default function CreateTeamModal() {
                 </div>
                 <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
                   <button
-                    type="button"
+                    type="submit"
                     className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
-                    onClick={() => {}}
                   >
                     Salvar
                   </button>
@@ -136,7 +138,7 @@ export default function CreateTeamModal() {
                     Cancel
                   </button>
                 </div>
-              </div>
+              </form>
             </Transition.Child>
           </div>
         </Dialog>

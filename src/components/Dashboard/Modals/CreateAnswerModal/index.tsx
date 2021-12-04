f5138d7949eb10adb6319c67d3c1d34d4b829fe0
carloseduardodb <carloseduardodiasbatista@gmail.com>
2021-12-04 16:18:26 -0300

    feat: add button submit in form

--- src/components/Dashboard/Modals/CreateAnswerModal/index.tsx
+++ src/components/Dashboard/Modals/CreateAnswerModal/index.tsx
@@ -251,10 +251,9 @@ export default function CreateAnswerModal() {
                 </div>
                 <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
                   <button
-                    type="button"
+                    type="submit"                  >
                     className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
-                    onClick={() => {}}
-                  >
+
                     Salvar
                   </button>
                   <button

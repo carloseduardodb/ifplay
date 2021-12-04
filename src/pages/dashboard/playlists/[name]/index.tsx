4dcd2fd01717e180565f202564357b9b149e7c7c
carloseduardodb <carloseduardodiasbatista@gmail.com>
2021-12-04 16:08:57 -0300

    feat: add separated item CardsVideos in dinamical page playlist

--- src/pages/dashboard/playlists/[name]/index.tsx
+++ src/pages/dashboard/playlists/[name]/index.tsx
@@ -60,45 +60,7 @@ const specific = () => {
         </div>
       </div>
       <div className="mx-16">
-        <div className="">
-          <h2 className="text-2xl text-white font-bold">Videos</h2>
-          <br />
-          <div className="bg-white rounded-md p-5 mb-10 w-96">
-            <label htmlFor="" className="font-semibold">
-              Nome do video
-            </label>
-            <p>Não sei o nome</p>
-            <br />
-            <label htmlFor="" className="font-semibold">
-              Url:
-            </label>
-            <br />
-            <a
-              className="text-blue-500"
-              href="https://www.youtube.com/watch?v=V3-dcP81lBQ"
-            >
-              https://www.youtube.com/watch?v=V3-dcP81lBQ
-            </a>
-            <div className="flex justify-end">
-              <button
-                className="w-full inline-flex justify-center rounded-md border 
-                          border-transparent shadow-sm px-4 py-2 bg-p-blue text-base font-medium 
-                        text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 
-                        focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
-              >
-                Editar
-              </button>
-              <button
-                className="w-full inline-flex justify-center rounded-md border 
-                          border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium 
-                        text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 
-                        focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
-              >
-                Apagar
-              </button>
-            </div>
-          </div>
-        </div>
+        <CardsVideos />
         <div className="">
           <h2 className="text-2xl text-white font-bold">Questões</h2>
           <br />

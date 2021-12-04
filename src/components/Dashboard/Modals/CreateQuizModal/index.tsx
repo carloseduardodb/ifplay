e11ff78a728e66282f5f5552b495ae6b752964ee
carloseduardodb <carloseduardodiasbatista@gmail.com>
2021-12-04 19:04:51 -0300

    feat: update format CreatQuizModal

--- src/components/Dashboard/Modals/CreateQuizModal/index.tsx
+++ src/components/Dashboard/Modals/CreateQuizModal/index.tsx
@@ -10,7 +10,7 @@ type Props = {
   setOpen: (open: boolean) => void;
 };
 
-export default function CreateQuizModal() {
+export default () => {
   const cancelButtonRef = useRef(null);
   const [open, setOpen] = useState(false);
   const handleViewModal = (name: string) => {
@@ -119,7 +119,6 @@ export default function CreateQuizModal() {
                   <button
                     type="submit"
                     className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
-                    onClick={() => {}}
                   >
                     Salvar
                   </button>
@@ -139,4 +138,4 @@ export default function CreateQuizModal() {
       </Transition.Root>
     </>
   );
-}
+};

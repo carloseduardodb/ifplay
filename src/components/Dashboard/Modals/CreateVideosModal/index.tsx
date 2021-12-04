23d6eb38ae55c2f13723f64c0a659fe57b52475a
carloseduardodb <carloseduardodiasbatista@gmail.com>
2021-12-04 19:06:21 -0300

    feat: add default methods for submid forms

--- src/components/Dashboard/Modals/CreateVideosModal/index.tsx
+++ src/components/Dashboard/Modals/CreateVideosModal/index.tsx
@@ -4,6 +4,8 @@ import { Dialog, Transition } from "@headlessui/react";
 import { HiSave } from "react-icons/hi";
 import { FiAperture } from "react-icons/fi";
 import api from "../../../../services/api";
+import router from "next/router";
+import { useDispatchGlobalEvent } from "../../../../hooks/useDispatchGlobalEvent";
 
 type Props = {
   open: boolean;
@@ -12,15 +14,23 @@ type Props = {
 
 export default function CreateVideosModal() {
   const cancelButtonRef = useRef(null);
+  const { dispatch, setDispatch } = useDispatchGlobalEvent();
   const [open, setOpen] = useState(false);
+  const [url, setUrl] = useState("");
   const handleViewModal = (name: string) => {
     setOpen(true);
   };
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setOpen(false);
     api
-      .post("/teacher/videos", {})
+      .post("/teacher/videos", {
+        url: url,
+        playlistId: router.query.quizId,
+      })
       .then((response) => {
         alert("Video criado com sucesso!");
+        setDispatch(!dispatch);
       })
       .catch((err) => {
         alert("Erro ao criar video!");
@@ -74,7 +84,10 @@ export default function CreateVideosModal() {
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+              <form
+                onSubmit={handleSubmit}
+                className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
+              >
                 <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                   <div className="sm:flex sm:items-start">
                     <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-blue-100 sm:mx-0 sm:h-10 sm:w-10">
@@ -92,7 +105,7 @@ export default function CreateVideosModal() {
                       </Dialog.Title>
                       <div className="mt-2">
                         <input
-                          onChange={(e) => {}}
+                          onChange={(e) => setUrl(e.target.value)}
                           id="playlist-name"
                           name="playlist-name"
                           type="text"
@@ -106,9 +119,8 @@ export default function CreateVideosModal() {
                 </div>
                 <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
                   <button
-                    type="button"
+                    type="submit"
                     className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
-                    onClick={() => {}}
                   >
                     Salvar
                   </button>
@@ -121,7 +133,7 @@ export default function CreateVideosModal() {
                     Cancel
                   </button>
                 </div>
-              </div>
+              </form>
             </Transition.Child>
           </div>
         </Dialog>

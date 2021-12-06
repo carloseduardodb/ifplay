aa3e247f903a5e58ba36a406dc4d67b94c4c0812
carloseduardodb <carloseduardodiasbatista@gmail.com>
2021-12-05 22:13:08 -0300

    refactor: modify name to component

--- src/pages/dashboard/answers/index.tsx
+++ src/pages/dashboard/answers/index.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import ResponsesAllPlaylists from "../../../components/Dashboard/ResponsesAllPlaylists/index";
+import HeaderStatsPlaylists from "../../../components/Dashboard/HeaderStatsPlaylists/index";
 
-const answers = () => {
-  return <div></div>;
+const responses = () => {
+  return (
+    <>
+      <HeaderStatsPlaylists />
+      <div className="w-full lg:-mt-4 px-5 mx-auto items-center justify-between md:flex-nowrap flex-wrap md:px-10 px-4">
+        <br />
+        <br />
+        <h1 className="text-xl font-semibold text-white">
+          Selecione uma Playlist
+        </h1>
+        <br />
+        <ResponsesAllPlaylists />
+      </div>
+    </>
+  );
 };
 
-export default answers;
+export default responses;

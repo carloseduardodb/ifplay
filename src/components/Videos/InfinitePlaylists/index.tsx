bab16ffbfd7c7d34dbaf9107dcef1ee3a5ad31c4
carloseduardodb <carloseduardodiasbatista@gmail.com>
2021-12-10 06:49:45 -0300

    feat: add infinity playlist

---
+++ src/components/Videos/InfinitePlaylists/index.tsx
@@ -0,0 +1,44 @@
+import { AxiosResponse } from "axios";
+import React, { useEffect } from "react";
+import { FaPlus } from "react-icons/fa";
+import api from "../../../services/api";
+import Video from "../../UI/Video";
+import Track from "../../UI/Track/index";
+
+interface TrackProps {
+  created_at: Date;
+  id: number;
+  name: string;
+  quiz_id: number;
+  teacher_id: number;
+  updated_at: Date;
+}
+
+const InfinitePlaylists = () => {
+  const [playlists, setPlaylists] = React.useState<TrackProps[]>([]);
+  useEffect(() => {
+    api
+      .get("playlist/all")
+      .then(({ data }: AxiosResponse) => {
+        setPlaylists(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <>
+      <p className="w-full text-3xl text-white font-bold mt-5 flex items-center">
+        <FaPlus size={20} className="text-p-yellow-light mx-3" /> Mais playlists
+      </p>
+      <div className="w-full grid xs:grid-cols-1 2xl:grid-cols-4 lg:grid-cols-5 xl:grid-cols-3 mb-10">
+        {playlists.map((playlist) => (
+          <Track data={playlist} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default InfinitePlaylists;

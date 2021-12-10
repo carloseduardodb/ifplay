df01e7f00f48578c24aa9f99e7b375fa085e2507
carloseduardodb <carloseduardodiasbatista@gmail.com>
2021-12-10 06:50:03 -0300

    refactor: updates component to stay dynamic

--- src/components/Videos/PlayTrack/ListVideos/index.tsx
+++ src/components/Videos/PlayTrack/ListVideos/index.tsx
@@ -1,29 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import DetailsVideoList from "./DetailsVideoList";
 import DetailsQuestions from "./DetailsQuestions";
+import api from "../../../../services/api";
+import { AxiosResponse } from "axios";
+import { useRouter } from "next/router";
+import { usePlaylist } from "../../../../hooks/usePlaylist";
 
 const ListVideos = () => {
+  const [videos, setVideos] = React.useState([]);
+  const router = useRouter();
+  const { playlist } = usePlaylist();
+  const [select, setSelect] = React.useState("");
+
   return (
     <ul className="w-4/12 overflow-hidden scrollbar scrollbar-thumb-p-green scrollbar-thin scrollbar-track-gray-800">
-      <DetailsVideoList selected />
-      <DetailsVideoList />
-      <DetailsVideoList />
-      <DetailsVideoList />
-      <DetailsVideoList />
-      <DetailsVideoList />
-      <DetailsVideoList />
-      <DetailsVideoList />
-      <DetailsVideoList />
-      <DetailsVideoList />
-      <DetailsVideoList />
-      <DetailsVideoList />
-      <DetailsVideoList />
-      <DetailsVideoList />
-      <DetailsVideoList />
-      <DetailsVideoList />
-      <DetailsVideoList />
-      <DetailsVideoList />
-      <DetailsQuestions />
+      {playlist?.videos?.map((video) => (
+        <DetailsVideoList url={video.url} />
+      ))}
     </ul>
   );
 };

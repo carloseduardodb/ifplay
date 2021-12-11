5ffe2f384acdaca1e2a2e1fc34bf70ae655d9788
carloseduardodb <carloseduardodiasbatista@gmail.com>
2021-12-11 14:40:14 -0300

    feat: check if the questionnaire is open

--- src/components/Videos/PlayTrack/index.tsx
+++ src/components/Videos/PlayTrack/index.tsx
@@ -2,15 +2,17 @@ import React from "react";
 import ListVideos from "./ListVideos";
 import PlayerVideo from "./PlayerVideo";
 import PlayQuestions from "./PlayQuestions";
+import { usePlaylist } from "../../../hooks/usePlaylist";
 
 const PlayTrack = () => {
+  const { isQuestions } = usePlaylist();
   return (
     <section
       style={{ height: "85vh" }}
       className="container my-5 flex justify-between"
     >
       <div className="w-8/12">
-        <PlayerVideo />
+        {!isQuestions ? <PlayerVideo /> : <PlayQuestions />}
       </div>
       <ListVideos />
     </section>

9b423a54e7890e60d993798220d668ddc3938c68
carloseduardodb <carloseduardodiasbatista@gmail.com>
2021-12-11 14:40:46 -0300

    feat: add top margin on component

--- src/components/Videos/InfiniteVideos/index.tsx
+++ src/components/Videos/InfiniteVideos/index.tsx
@@ -29,7 +29,7 @@ const InfiniteVideos = () => {
 
   return (
     <>
-      <p className="w-full text-3xl text-white font-bold mt-5 flex items-center">
+      <p className="w-full text-3xl text-white font-bold flex items-center">
         <FaPlus size={20} className="text-p-yellow-light mx-3" /> Mais videos
       </p>
       <div className="w-full grid xs:grid-cols-1 2xl:grid-cols-4 lg:grid-cols-5 xl:grid-cols-3 mb-10">

e19ff20f54dabab73b34efe665f132cb554ef358
carloseduardodb <carloseduardodiasbatista@gmail.com>
2021-12-11 14:38:38 -0300

    fix: bug when component state is not loaded

--- src/components/Videos/PlayTrack/PlayerVideo/index.tsx
+++ src/components/Videos/PlayTrack/PlayerVideo/index.tsx
@@ -1,10 +1,8 @@
 import React from "react";
-import PlayQuestions from "../PlayQuestions";
-import ListVideos from "./../ListVideos";
 import { usePlaylist } from "../../../../hooks/usePlaylist";
 
 const PlayerVideo = () => {
-  const { selectPlaylist } = usePlaylist();
+  const { selectPlaylist, playlist } = usePlaylist();
   // pega a chave de um link de video do youtube
   function getId(url: string) {
     const regExp =
@@ -13,28 +11,25 @@ const PlayerVideo = () => {
     return match && match[2].length === 11 ? match[2] : null;
   }
 
-  return (
-    <iframe
-      width="100%"
-      height="80%"
-      src={`https://www.youtube.com/embed/${getId(
-        selectPlaylist
-      )}?modestbranding=1&rel=0&showinfo=0`}
-      title="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
-    ></iframe>
-  );
-};
-
-/** código do video
- * 
+  if (playlist) {
+    return (
+      <>
         <iframe
           width="100%"
-          height="100%"
-          src="https://www.youtube.com/embed/hbTvThjKlUk?modestbranding=1&rel=0&showinfo=0"
+          height="80%"
+          src={`https://www.youtube.com/embed/${
+            selectPlaylist
+              ? getId(selectPlaylist)
+              : getId(playlist.videos[0].url)
+          }?modestbranding=1&rel=0&showinfo=0`}
           title="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
         ></iframe>
- */
+      </>
+    );
+  } else {
+    return <></>;
+  }
+};
 
 export default PlayerVideo;

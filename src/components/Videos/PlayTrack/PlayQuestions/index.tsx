37b4a445fb7ea756ba682cadb211c6373f1a1291
carloseduardodb <carloseduardodiasbatista@gmail.com>
2021-12-11 14:37:48 -0300

    feat: manager to routes inside component

--- src/components/Videos/PlayTrack/PlayQuestions/index.tsx
+++ src/components/Videos/PlayTrack/PlayQuestions/index.tsx
@@ -1,8 +1,24 @@
 import React from "react";
 import SurveyComponent from "./SurveyComponent";
+import Question from "./SurveyComponent/Question";
+import Finally from "./SurveyComponent/Finally";
+import ViewResponses from "./SurveyComponent/ViewResponses";
+import { usePlaylist } from "../../../../hooks/usePlaylist";
 
 const PlayQuestions = () => {
-  return <SurveyComponent />;
+  const { questionStatusScreen } = usePlaylist();
+  if (questionStatusScreen === "survey") {
+    return <SurveyComponent />;
+  }
+  if (questionStatusScreen === "question") {
+    return <Question />;
+  }
+  if (questionStatusScreen === "finally") {
+    return <Finally />;
+  }
+  if (questionStatusScreen === "responses") {
+    return <ViewResponses />;
+  }
 };
 
 export default PlayQuestions;

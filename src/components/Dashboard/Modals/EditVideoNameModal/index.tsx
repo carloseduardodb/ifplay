7ac5f324b97bb84c8fe62e7486056aabb651ee77
carloseduardodb <carloseduardodiasbatista@gmail.com>
2021-12-12 01:26:08 -0300

    feat: add component EditVideoNameModal

---
+++ src/components/Dashboard/Modals/EditVideoNameModal/index.tsx
@@ -0,0 +1,157 @@
+/* This example requires Tailwind CSS v2.0+ */
+import { Fragment, useRef, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { HiSave } from "react-icons/hi";
+import { FiAperture } from "react-icons/fi";
+import api from "../../../../services/api";
+import router from "next/router";
+import { useDispatchGlobalEvent } from "../../../../hooks/useDispatchGlobalEvent";
+import { toast } from "react-toastify";
+
+type Props = {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+};
+
+export default function EditVideoNameModal() {
+  const cancelButtonRef = useRef(null);
+  const { dispatch, setDispatch } = useDispatchGlobalEvent();
+  const [open, setOpen] = useState(false);
+  const [url, setUrl] = useState("");
+  const handleViewModal = (name: string) => {
+    setOpen(true);
+  };
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setOpen(false);
+    /* verificar se a url é do youtube */
+    const regex =
+      /^(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
+    const match = url.match(regex);
+    if (!match) {
+      alert("url inválida");
+      return;
+    }
+
+    if (url.length > 0) {
+      api
+        .post("/teacher/videos", {
+          url: url,
+          playlistId: router.query.quizId,
+        })
+        .then((response) => {
+          toast.success("Video criado com sucesso!");
+          setDispatch(!dispatch);
+        })
+        .catch((err) => {
+          toast.error("Erro ao criar video!");
+        });
+    }
+  };
+
+  return (
+    <>
+      <button
+        onClick={() => {
+          handleViewModal("create-questions");
+        }}
+        className="w-full inline-flex justify-center rounded-md border 
+                    border-transparent shadow-sm px-4 py-2 bg-p-blue text-base font-medium 
+                  text-white hover:bg-p-blue-dark focus:outline-none focus:ring-2 focus:ring-offset-2 
+                  focus:ring-p-blue-dark sm:ml-3 sm:w-auto sm:text-sm"
+      >
+        Editar
+      </button>
+      <Transition.Root show={open} as={Fragment}>
+        <Dialog
+          as="div"
+          className="fixed z-10 inset-0 overflow-y-auto"
+          initialFocus={cancelButtonRef}
+          onClose={setOpen}
+        >
+          <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            </Transition.Child>
+
+            {/* This element is to trick the browser into centering the modal contents. */}
+            <span
+              className="hidden sm:inline-block sm:align-middle sm:h-screen"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <form
+                onSubmit={handleSubmit}
+                className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
+              >
+                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                  <div className="sm:flex sm:items-start">
+                    <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-blue-100 sm:mx-0 sm:h-10 sm:w-10">
+                      <HiSave
+                        className="h-6 w-6 text-blue-600"
+                        aria-hidden="true"
+                      />
+                    </div>
+                    <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                      <Dialog.Title
+                        as="h3"
+                        className="text-lg leading-6 font-medium text-gray-900"
+                      >
+                        Adicione aqui o link do video
+                      </Dialog.Title>
+                      <div className="mt-2">
+                        <input
+                          onChange={(e) => setUrl(e.target.value)}
+                          id="playlist-name"
+                          name="playlist-name"
+                          type="text"
+                          required
+                          className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                          placeholder="Link do seu video"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                  <button
+                    type="submit"
+                    className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
+                  >
+                    Atualizar
+                  </button>
+                  <button
+                    type="button"
+                    className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                    onClick={() => setOpen(false)}
+                    ref={cancelButtonRef}
+                  >
+                    Cancelar
+                  </button>
+                </div>
+              </form>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition.Root>
+    </>
+  );
+}

85d7c95eb62a8199134a18353471feae2bf6ff2a
carloseduardodb <carloseduardodiasbatista@gmail.com>
2021-12-12 17:20:57 -0300

    refactor: modify slide button position

--- src/components/Dashboard/CardsTeams/index.tsx
+++ src/components/Dashboard/CardsTeams/index.tsx
@@ -49,7 +49,7 @@ const CardsTeams = () => {
           showStatus={false}
           renderArrowPrev={(onClickHandler, hasPrev, label) => (
             <button
-              className={`bg-p-green absolute bottom-14 z-10 left-2 hover:bg-p-green text-white font-semibold hover:text-white py-3 px-1 border border-p-green hover:border-transparent rounded ${
+              className={`bg-p-green absolute bottom-32 z-10 left-2 hover:bg-p-green text-white font-semibold hover:text-white py-3 px-1 border border-p-green hover:border-transparent rounded ${
                 !hasPrev && "hidden"
               }`}
               onClick={() => {
@@ -63,7 +63,7 @@ const CardsTeams = () => {
           )}
           renderArrowNext={(onClickHandler, hasNext, label) => (
             <button
-              className={`bg-p-green absolute bottom-14 right-2 hover:bg-p-green text-white font-semibold hover:text-white py-3 px-1 border border-p-green hover:border-transparent rounded ${
+              className={`bg-p-green absolute bottom-32 right-2 hover:bg-p-green text-white font-semibold hover:text-white py-3 px-1 border border-p-green hover:border-transparent rounded ${
                 !hasNext && "hidden"
               }`}
               onClick={() => {

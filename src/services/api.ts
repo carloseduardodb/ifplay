8241610c54994f39b6156af5392645a78dab29fc
carloseduardodb <carloseduardodiasbatista@gmail.com>
2022-01-30 15:47:25 -0300

    feat: redirect requests to heroku server

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:3333",
+  baseURL: "https://ifplay.herokuapp.com/",
 });
 
 export default api;

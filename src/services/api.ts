f800a676a957c4d9db8cbf3bbcc48a33e7b4d28d
carloseduardodb <carloseduardodiasbatista@gmail.com>
2022-02-27 13:38:51 -0300

    feat: update cors origin

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,10 +2,6 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://ifplay.herokuapp.com/",
-  headers: {
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "ifplay.herokuapp.com",
-  },
   // baseURL: "http://localhost:3333",
 });
 
